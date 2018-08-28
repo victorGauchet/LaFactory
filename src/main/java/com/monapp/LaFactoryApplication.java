f9c390ab358decd696370802837ed88680414134
Unknown <nmalquin@gmail.com>
2018-08-28 11:36:01 +0200

    Premier commit
    Création du projet

---
+++ src/main/java/com/monapp/LaFactoryApplication.java
@@ -0,0 +1,12 @@
+package com.monapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaFactoryApplication.class, args);
+	}
+}
